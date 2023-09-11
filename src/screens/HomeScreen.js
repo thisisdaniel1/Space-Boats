7f8f9d046cbf849122381ce7204d028f585f6690
Daniel Nguyen <dknguye5@uno.edu>
2023-09-11 12:21:31 -0500

    Initial Commit
    Barebones app to start with. Based on the rnstarter app but stripped down even more.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet} from "react-native";
+
+const HomeScreen = () => {
+
+  return  <View>
+    </View>
+  };
+
+  const styles = StyleSheet.create({});
+
+export default HomeScreen;
